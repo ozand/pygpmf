278d68d95e63c6138d2bac4e97dbb6d8f34c5c12
ozand <ozand.ru@gmail.com>
2026-01-12 13:44:50 +0300

    Docs: Update version to 0.3.2 and ensure ReadTheDocs config

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2026, Ozan Degirmenci'
 author = 'Ozan Degirmenci'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
